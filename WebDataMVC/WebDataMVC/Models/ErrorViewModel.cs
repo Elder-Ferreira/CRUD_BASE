74be3002f1d2f10a5f3fe1cc8e7e88c63be8c79e
Elder-Ferreira <ferreira3.elder@gmail.com>
2023-12-15 18:52:40 -0300

    Criado WebDataMVC

---
+++ WebDataMVC/WebDataMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebDataMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
